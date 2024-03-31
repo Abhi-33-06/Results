52ec0455a2c6a649b224c8b3f1a32b98d4128581
Abhi-33-06 <165570489+Abhi-33-06@users.noreply.github.com>
2024-03-31 20:01:18 +0530

    Add files via upload

---
+++ prankss.js
@@ -0,0 +1,6 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+  document.getElementById('aprilFoolsMessage').style.display = 'block';
+  this.style.display = 'none'; // Hide the button after it's clicked
+});
+
+
